f1a7a54f13e1948979c15fd1d59bef050104e82f
BMDan <881372+BMDan@users.noreply.github.com>
2024-03-21 10:20:00 -0700

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { API } from 'homebridge';
+import { API, Logger, Logging } from 'homebridge';
 import { GarageDoor, initESPHome } from './garage-door';
 import { withPrefix } from 'homebridge/lib/logger';
 
 export default function(api: API) {
+  //logger = Logger.withPrefix('ESPHome');
+  // @//ts-ignore
+  let logga = withPrefix('ESPHome');
+  logga.error('HRE registering (l.i)...');
+  //throw new Error("Well, shit.");
   api.registerAccessory('homebridge-ratgdo-esphome', 'GarageDoor', GarageDoor);
+  logga.error('HRE registered (l.i)...');
 }
 
 function run() {
+  let logger = withPrefix('ESPHome');
+  logger.error('HRE running (l.i)...');
   const source = initESPHome('192.168.0.50', 80, withPrefix('ESPHome'), e => console.log(e));
   return new Promise<void>(resolve => {
     process.stdin.on('keypress', async (str, key) => {
